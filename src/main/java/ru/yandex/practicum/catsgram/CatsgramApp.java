812241d03a710594a84fc3b55df144f51a13af39
VladimirTamarov <vover134@yandex.ru>
2024-06-23 23:20:33 +0400

    Новый проект

---
+++ src/main/java/ru/yandex/practicum/catsgram/CatsgramApp.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.catsgram;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+import java.util.Map;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class CatsgramApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CatsgramApp.class, args);
+
+}}
